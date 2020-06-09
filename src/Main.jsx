6d7f24e3aedbc6612b0d16e572a84b64a0efd35d
Rabii Luena <itisrabii@gmail.com>
2020-06-09 13:18:52 +0300

    feat: adding first sample component

---
+++ src/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
+
+const Main = () => {
+  return (
+    <View>
+      <AddEntry />
+    </View>
+  );
+};
+
+export default Main;
---
+++ src/components/AddEntry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { getMetricMetaInfo } from '../utils/helper';
+
+export default () => {
+    return(
+        <View>
+            <Text>Add Entry</Text>
+        </View>
+    )
+}
