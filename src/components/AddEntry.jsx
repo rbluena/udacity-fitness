926342ea61f237f4fbe345a5112c8c0e9e85ea15
Rabii Luena <itisrabii@gmail.com>
2020-06-09 15:21:40 +0300

    fix: fix eslint errors and warnings

--- src/components/AddEntry.jsx
+++ src/components/AddEntry.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helper';
 
-export default () => {
-    return(
-        <View>
-            <Text>Add Entry</Text>
-        </View>
-    )
-}
+const AddEntry = () => {
+  return (
+    <View>
+      <Text>Add Entry</Text>
+    </View>
+  );
+};
+
+export default AddEntry;
