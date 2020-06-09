198c476d780e03e125eac5bcbaa76e71aee26df6
Rabii Luena <itisrabii@gmail.com>
2020-06-09 16:30:55 +0300

    chore: adding babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
+const path = require('path');
+
+const plugins = [
+  [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: [path.resolve('.')],
+      extensions: ['.js', '.jsx', '.json', '.android.js', '.android.jsx'],
+      alias: {
+        '@app-screens': './app/screens',
+        '@app-components': './app/components',
+        '@app-utils': './app/utils',
+      },
+    },
+  ],
+];
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: plugins,
   };
 };
